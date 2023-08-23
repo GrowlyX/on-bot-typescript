f97cc05477883d2be5b2e42a89c1cc71beb04d72
GrowlyX <growlyxdev@gmail.com>
2023-08-23 17:27:51 -0400

    chore: remove script-util

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,8 +6,7 @@ dependencies {
     api(kotlin("compiler-embeddable"))
 
     compileOnly(fileTree("ftc-libs"))
-    // TODO: compat issues with Android? Do we need coroutines-android?
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.reflections:reflections:0.10.2")
 
     runtimeOnly(kotlin("scripting-compiler-embeddable"))
