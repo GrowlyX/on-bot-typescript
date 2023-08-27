8e7ee9dd5c448c159cfa26bff08ef56642def9de
hextanium <hextanium@my.pc>
2023-08-27 00:56:55 -0400

    refactor: remove heading

--- frontend/src/components/FileDisplay/FileTree.svelte
+++ frontend/src/components/FileDisplay/FileTree.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-    import {isFile} from "$lib/files/isFile";
-    import {merge, pathsToTree} from "$lib/files/pathsToTree";
+    import {pathsToTree} from "$lib/files/pathsToTree";
     import Folder from "./Folder.svelte";
     import {files} from "../../stores";
     import type {TFile} from "$lib/files/TFile";
@@ -14,8 +13,5 @@
 </script>
 
 <ul class="p-5 m-5 rounded-md menu bg-zinc-700">
-    <h1 class="text-center font-bold text-white">Your Scripts:</h1>
-    <!-- TODO: map on each node of `root` instead of one big root element because that's scuffed -->
-
     <Folder name="" files={$root} isRoot />
 </ul>
