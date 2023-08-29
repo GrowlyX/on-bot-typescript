5b3cbf5733580bd4f2e3483f5832aa4573797b54
TechnoDrive <hextanium.on.discord@gmail.com>
2023-08-29 14:32:13 -0400

    refactor: move the `Toast` component to be globally available across all routes

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
     import "../app.css";
     import MetaTitle from "../components/Meta/MetaTitle.svelte";
+    import Toast from "../components/Toast.svelte"
 </script>
 
 <slot />
+<Toast/>
 <MetaTitle _title="Home" />
