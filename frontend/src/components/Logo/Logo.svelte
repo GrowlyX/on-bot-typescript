c7e57854f3d70255e3d1aa95fec54c76cf1f0705
GrowlyX <growlyxdev@gmail.com>
2023-08-29 18:07:35 -0400

    style: add hover transitioning and padding to the Logo

--- frontend/src/components/Logo/Logo.svelte
+++ frontend/src/components/Logo/Logo.svelte
@@ -4,8 +4,8 @@ import {viewingScript} from "../../stores.ts";
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div on:click={() => { viewingScript.set(null) }} class="flex flex-col justify-center hover:bg">
-    <img class="logo cursor-pointer hover:bg-[#2a2a2a] rounded-md scale-75"
+<div on:click={() => { viewingScript.set(null) }} class="flex flex-col justify-center hover:bg-[#2a2a2a] transition-colors rounded-md m-4 cursor-pointer">
+    <img class="logo scale-75"
          src="logo.png" id="logo" alt="OnBot Kotlin Logo">
 </div>
 
